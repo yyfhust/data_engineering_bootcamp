6f9a3a5d9adee926bf5ddc6544d1dc386bdbe144
Yifan Ye <yifan@vionlabs.com>
2022-05-19 16:25:58 +0200

    dockerize ingeston code

--- week1/docker-postgres/command.sh
+++
@@ -1,9 +0,0 @@
-
-
-docker run -it \
-  -e POSTGRES_USER="root" \
-  -e POSTGRES_PASSWORD="root" \
-  -e POSTGRES_DB="ny_taxi" \
-  -v $(pwd)/ny_taxi:/var/lib/postgresql/data \
-  -p 5432:5432 \
-  postgres:13
