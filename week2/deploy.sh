3643f9832205a58e8c9ec19092afd73c69c34ad6
Yifan Ye <yifan@vionlabs.com>
2022-05-23 16:28:40 +0200

    airflow

--- week2/deploy.sh
+++ week2/deploy.sh
@@ -1,7 +1,7 @@
 
 
 
-airflow_bucket="gs://us-east1-gfp-e88fa259-bucket"
+airflow_bucket=""
 
 gsutil -o 'GSUtil:parallel_process_count=1' -m cp -r  "${airflow_bucket}/dags"  "${airflow_bucket}/dags.last"
 gsutil -o 'GSUtil:parallel_process_count=1' -m cp -r dags "${airflow_bucket}"
