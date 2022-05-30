85a26f315eac502a6c0352e309f1f164e035a6f2
Yifan Ye <yifan@vionlabs.com>
2022-05-30 15:49:38 +0200

    data ingestion

--- week2/dags/data_ingestion.py
+++ week2/dags/data_ingestion.py
@@ -17,40 +17,11 @@ import pyarrow.parquet as pq
 
 PROJECT_ID = cf.PROJECT_ID
 BUCKET = cf.BUCKET
+GCS_URL = cf.GCS_URL
 dataset_url = cf.DATASET_URL + cf.DATASET_FILE
-path_to_local_home = cf.AIRFLOW_HOME
-parquet_file = cf.DATASET_FILE.replace('.csv', '.parquet')
 BIGQUERY_DATASET = cf.BIGQUERY_DATASET
 DATASET_FILE = cf.DATASET_FILE
 
-def format_to_parquet(src_file, dst_file):
-    if not src_file.endswith('.csv'):
-        logging.error("Can only accept source files in CSV format, for the moment")
-        return
-    table = pv.read_csv(src_file)
-    pq.write_table(table, dst_file)
-
-
-# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
-def upload_to_gcs(bucket, object_name, local_file):
-    """
-    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
-    :param bucket: GCS bucket name
-    :param object_name: target path & file-name
-    :param local_file: source path & file-name
-    :return:
-    """
-    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-    # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
-    # End of Workaround
-
-    client = storage.Client()
-    bucket = client.bucket(bucket)
-
-    blob = bucket.blob(object_name)
-    blob.upload_from_filename(local_file)
 
 
 default_args = {
@@ -77,27 +48,7 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{cf.DATASET_FILE}"
-    )
-
-    format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{path_to_local_home}/{cf.DATASET_FILE}",
-            "dst_file": f"{path_to_local_home}/{cf.parquet_file}"
-        },
-    )
-
-    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
-        },
+        bash_command=f"curl -sSL {dataset_url} > {DATASET_FILE}  && gsutil cp {DATASET_FILE} {GCS_URL}"  
     )
 
     bigquery_external_table_task = BigQueryCreateExternalTableOperator(
@@ -110,9 +61,9 @@ with DAG(
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
+                "sourceUris": [f"gs://{BUCKET}/{DATASET_FILE}"],
             },
         },
     )
 
-    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
+    download_dataset_task  >> bigquery_external_table_task
